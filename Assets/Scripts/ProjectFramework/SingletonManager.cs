8a6385c9aaf0282a488d336de71dfe20a66d4d3a
AndKnow <495609155@qq.com>
2023-12-04 23:35:15 +0800

    feat：单例管理器的初始化，分为单次和多次，主要是在场景切换的时候

--- Assets/Scripts/ProjectFramework/SingletonManager.cs
+++ Assets/Scripts/ProjectFramework/SingletonManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace FrameWork
 {
-    public class SingletonManager<T> where T : new()
+    public abstract class SingletonManager<T> where T : new()
     {
         protected static T _instance;
         public static T Instance 
@@ -20,6 +22,29 @@ namespace FrameWork
             {
                 _instance = value;
             }
+        }
+
+        protected static bool _hasInitialized;
+        public UniTask Initialize()
+        {
+            UniTask task1 = UniTask.CompletedTask, task2 = UniTask.CompletedTask;
+            if (!_hasInitialized)
+            {
+                _hasInitialized = true;
+                task1 = OnlyOnceInit();
+            }
+            task2 = MultipleInit();
+            return UniTask.WhenAll(task1, task2);
+        }
+
+        protected virtual UniTask OnlyOnceInit()
+        {
+            return UniTask.CompletedTask;
+        }
+
+        protected virtual UniTask MultipleInit()
+        {
+            return UniTask.CompletedTask;
         }
     }
 
