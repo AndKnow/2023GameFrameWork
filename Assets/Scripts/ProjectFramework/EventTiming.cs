beb960e113b8b0ea497056ce3f8306d86922ef92
AndKnow <495609155@qq.com>
2023-12-18 21:47:30 +0800

    feat：定义命名类，防止在使用string约定的时候出现偏差

---
+++ Assets/Scripts/ProjectFramework/EventTiming.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class EventTiming
+{
+    public class TestTiming
+    {
+        public const string timing1 = "timing1";
+        public const string timing2 = "timing2";
+    }
+} 
